4644bb88d570b83ea1bea1a39c24958a2a139111
Chetan <chetansirsa@gmail.com>
2020-07-07 19:53:43 +0530

    Added Fruits Api

---
+++ FruitsApplication.java
@@ -0,0 +1,15 @@
+package com.example.Fruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsApplication.class, args);
+	}
+
+}
+
+
